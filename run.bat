1b95f99d4b5e37926b4af33ee54f8cc83c945a51
esteban201483 <Esteban201483@gmail.com>
2019-07-15 19:08:54 -0600

    Compilador genera codigo correcto

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 java -jar lib/java-cup-11b.jar -locations -interface -parser Parser   Parser.cup
-javac -cp java-cup-11b-runtime.jar *.java
-java  -cp java-cup-11b-runtime.jar -cp . main test/Prueba.txt
+javac -cp lib/java-cup-11b-runtime.jar *.java
+java  -cp lib/java-cup-11b-runtime.jar -cp . main test/Prueba.txt
